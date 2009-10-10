2c5b26e6e35a3df098900fd95dd2d55b490602a3
Jacques Crocker <merbjedi@gmail.com>
2009-10-10 04:42:09 -0700

    MerbAdapter Fixes (from Pavel Kunc)

--- lib/webrat/adapters/merb.rb
+++ lib/webrat/adapters/merb.rb
@@ -1,10 +1,7 @@
-require "webrat/integrations/merb"
-
 module Webrat
   class MerbAdapter < RackAdapter #:nodoc:
     def initialize(context=nil)
-      app = context.respond_to?(:app) ?
-        context.app : Merb::Rack::Application.new
+      app = context.respond_to?(:app) ? context.app : Merb::Config[:app]
       super(Rack::Test::Session.new(Rack::MockSession.new(app, "www.example.com")))
     end
   end
